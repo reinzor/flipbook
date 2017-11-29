4552ec32c0e830dfcb9b9ee875551a0b1f72e81b
Rein Appeldoorn <reinzor@gmail.com>
2017-11-29 22:52:16 +0100

    Writing image reducing frame rate works, pdf writing not yet

--- flipbook_from_video.py
+++ flipbook_from_video.py
@@ -4,6 +4,8 @@ import argparse
 import os
 import sys
 import cv2
+import Image
+from fpdf import FPDF
 
 
 def is_valid_file(parser, arg):
@@ -12,28 +14,34 @@ def is_valid_file(parser, arg):
     else:
         return arg
 
+def get_flipbook_frames(flipbook_fps, video_file_path):
+    cap = cv2.VideoCapture(video_file_path)
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    video_dt = 1./fps
+    flipbook_dt = 1./args.fps
+
+    video_t = 0
+    flipbook_t = 0
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if ret:
+            if video_t >= flipbook_t: 
+                yield frame
+                flipbook_t += flipbook_dt
+
+            video_t += video_dt
+        else:
+            break
+
+
 parser = argparse.ArgumentParser()
 parser.add_argument("video", help="Input video for the flipbook", type=lambda x: is_valid_file(parser, x))
-parser.add_argument("--fps", help="How many frames (images) per second of video", default=20)
+parser.add_argument("--fps", type=float, help="How many frames (images) per second of video", default=20)
 
 
 args = parser.parse_args()
-cap = cv2.VideoCapture(args.video)
-fps = cap.get(cv2.cv.CV_CAP_PROP_FPS)
 
-print "FPS (video={}, flipbook={})".format(fps, args.fps)
-
-# Now loop over the all images in the video and collect the flipbook frames
-flipbook_frames = []
-while cap.isOpened():
-    ret, frame = cap.read()
-
-    if not ret:
-        print "Failed to read frame from video file capture!"
-        sys.exit(1)
-
-    cv2.imshow('video', frame)
-    cv2.waitKey(1)
-
-cap.release()
-cv2.destroyAllWindows()
+for i, frame in enumerate(get_flipbook_frames(args.fps, args.video)):
+    tmp_frame_path = '/tmp/tmp_{}.jpeg'.format(i)
+    print "Writing image to tmp path {}".format(tmp_frame_path)
+    cv2.imwrite(tmp_frame_path, frame)

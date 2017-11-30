868bddbcd51df6d70e915c78fd5d9bbd05efac87
Rein Appeldoorn <reinzor@gmail.com>
2017-11-30 17:34:44 +0100

    Last fixes with margin and label added

--- flipbook_from_video.py
+++ flipbook_from_video.py
@@ -40,16 +40,21 @@ def get_flipbook_frames(flipbook_fps, video_file_path, padding):
 
     video_t = 0
     flipbook_t = 0
+    frame_id = 0
     while cap.isOpened():
+        frame_id += 1
         ret, video_frame = cap.read()
         if ret:
             if video_t >= flipbook_t:
                 h, w = video_frame.shape[:2]
                 result_img = np.zeros((h, w + padding, 3), np.uint8)
-                result_img[:h, :w] = video_frame
+                result_img[:h, :padding] = (255, 255, 255)
+                result_img[:h, padding:padding+w] = video_frame
 
                 cv2.rectangle(result_img, (0, 0), (w + padding - 1, h - 1), (0, 0, 0), 3)
                 cv2.rectangle(result_img, (0, 0), (padding, h - 1), (0, 0, 0), 3)
+                cv2.putText(result_img, str(frame_id), (padding / 2 - 10, h / 2),
+                            cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0))
 
                 yield result_img
                 flipbook_t += flipbook_dt
@@ -61,11 +66,13 @@ def get_flipbook_frames(flipbook_fps, video_file_path, padding):
 
 parser = argparse.ArgumentParser()
 parser.add_argument("video", help="Input video for the flipbook", type=lambda x: is_valid_file(parser, x))
-parser.add_argument("--fps", type=float, help="How many frames (images) per second of video", default=20)
+parser.add_argument("--fps", type=float, help="How many frames (images) per second of video", default=24)
 parser.add_argument("--dpi", type=int, help="How many pixels per inch", default=75)
 parser.add_argument("--num-cols", type=int, help="How many columns", default=2)
 parser.add_argument("--output", type=str, help="Output PDF filename", default=DEFAULT_FILENAME)
-parser.add_argument("--padding-left", type=float, help="Padding in inches from the left", default=1)
+parser.add_argument("--padding-left", type=float, help="Padding in inches from the left", default=2)
+parser.add_argument("--margin-x", type=float, help="Margin x around page in inches", default=.3)
+parser.add_argument("--margin-y", type=float, help="Margin y around page in inches", default=1)
 
 
 args = parser.parse_args()
@@ -73,6 +80,8 @@ args = parser.parse_args()
 a4_width = 8.27 * args.dpi
 a4_height = a4_width * math.sqrt(2)
 padding_left_pixels = int(args.padding_left * args.dpi)
+margin_x_pixels = int(args.margin_x * args.dpi)
+margin_y_pixels = int(args.margin_y * args.dpi)
 
 c = canvas.Canvas(args.output)
 c.setPageSize((a4_width, a4_height))
@@ -86,17 +95,16 @@ for i, frame in enumerate(get_flipbook_frames(args.fps, args.video, padding_left
     # #####
     frame_height, frame_width = frame.shape[:2]
     ratio = float(frame_width) / frame_height
-    width = a4_width / args.num_cols
-    height = width * ratio
-    num_rows = int(a4_height / height)
+    width = (a4_width - 2 * margin_x_pixels) / args.num_cols
+    height = width / ratio
+    num_rows = math.floor((a4_height - 2 * margin_y_pixels) / height)
     per_page = num_rows * args.num_cols
     # #####
 
     offset_x = (i % args.num_cols) * width
     offset_y = int((i % per_page) / args.num_cols) * height
 
-    # print "idx={}\t offset_x:{}\t offset_y={}".format(i, offset_x, offset_y)
-    c.drawImage(tmp_frame_path, offset_x, offset_y, width, height)
+    c.drawImage(tmp_frame_path, margin_x_pixels + offset_x, margin_y_pixels + offset_y, width, height)
 
     if (i + 1) % per_page == 0:
         c.showPage()
